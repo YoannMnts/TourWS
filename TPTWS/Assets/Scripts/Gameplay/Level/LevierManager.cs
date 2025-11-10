f9565818e2cfbcbfe1d7602faa71ec5a7983994c
KaKoKa5 <bonfardeci.saverio@e-artsup.net>
2025-11-10 19:05:45 +0100

    Levier+Porte ouvrante

---
+++ TPTWS/Assets/Scripts/Gameplay/Level/LevierManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+
+namespace TPT.Gameplay.Level
+{
+    public class LevierManager : MonoBehaviour
+    {
+                [SerializeField] private Levier[] leviers;
+                [SerializeField] private UnityEvent<int, int> OnLevierChange;
+        
+                [SerializeField] private DoorMove waterController;
+        
+                private void OnEnable()
+                {
+                        foreach (Levier levier in leviers)
+                        {
+                                levier.OnLevierChange += CheckLeviers;
+                        }
+                }
+        
+                private void OnDisable()
+                {
+                        foreach (Levier levier in leviers)
+                        {
+                                levier.OnLevierChange -= CheckLeviers;
+                        }
+                }
+        
+                private void Start()
+                {
+                        CheckLeviers();
+                }
+        
+                private void CheckLeviers()
+                {
+                        int active = 0;
+                        foreach (Levier levier in leviers)
+                        {
+                                if (levier.isActive)
+                                        active++;
+                        }
+        
+                        // 🔹 Baisse du niveau de l’eau selon le nombre de leviers activés
+                        if (waterController != null)
+                        {
+                                float normalized = (float)active / leviers.Length;
+                                waterController.SetWaterLevel(normalized);
+                        }
+        
+                        OnLevierChange?.Invoke(active, leviers.Length);
+                }
+        }
+    }
+
