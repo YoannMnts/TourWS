0a0bd1d874687029e59593638b21d574954ec661
KaKoKa5 <bonfardeci.saverio@e-artsup.net>
2025-11-10 19:53:28 +0100

    Relique magique
    Je fais mais c'est à valider

---
+++ TPTWS/Assets/Scripts/Gameplay/Level/Relique.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using  TPT.Core.Core.HeroData;
+
+namespace TPT.Gameplay.Level
+{
+    public class Relique : MonoBehaviour
+    {
+        [SerializeField] private KeyCode interactKey = KeyCode.E;
+        private HeroData hero;
+        private bool playerInRange = false;
+        void Update()
+        {
+            if (playerInRange && Input.GetKeyDown(interactKey))
+            {
+                Destroy(gameObject);
+            }
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                playerInRange = true;
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                playerInRange = false;
+            }
+        }
+
+        private void Strenght()
+        {
+            
+        }
+        private void Health()
+        {
+            
+        }
+        private void ()
+        {
+            
+        }
+    }
+}
